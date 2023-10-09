0d9b45236b8884eb8318db1e3c6a3801eb508aeb
Nguyễn Bảo Ngọc <91652961+ngocj@users.noreply.github.com>
2023-10-09 09:41:03 +0700

    Trang chủ
    Trang chủ

---
+++ trang chu.jsx
@@ -0,0 +1,106 @@
+
+
+export const HomePage = () => {
+    return (
+        <div>
+            {/*CONTENT */}
+            <div>
+                <img className="w-full h-screen shadow-md pt-16" src="https://theme.hstatic.net/1000150581/1001058888/14/slider_2.jpg?v=277" alt="" />
+            </div>
+            <div>
+                <h1 className="text-center font-semibold text-3xl py-10">SPORT BASKETBALL</h1>
+                <div className="flex px-10">
+                    <img className=" hover:scale-110 cursor-pointer duration-300 ease-linear rounded-md w-1/3 px-5 h-50" src="https://theme.hstatic.net/1000150581/1001058888/14/season_coll_1_img_large.png?v=277" alt="" />
+                    <img className=" hover:scale-110 cursor-pointer duration-300 ease-linear rounded-md w-1/3 px-5 h-50" src="https://theme.hstatic.net/1000150581/1001058888/14/season_coll_2_img_large.png?v=277" alt="" />
+                    <img className=" hover:scale-110 cursor-pointer duration-300 ease-linear rounded-md w-1/3 px-5 h-50" src="https://theme.hstatic.net/1000150581/1001058888/14/season_coll_3_img_large.png?v=277" alt="" />
+                </div>
+            </div>
+            <h1 className=" px-14 font-semibold text-3xl pt-20">SẢN PHẨM BÁN CHẠY</h1>
+            <div className="flex px-12">
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812315555-4__9c40d8019bfb465c9a5777ee69e8b543_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày chạy thể thao nam A-SILO Anta 812315555-4</a>
+                    <p className="text-red-500">860,000₫</p>
+                    <s>1,599,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-47%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812317719-7__16__fd3d33d7b72441ce92294528bdf1e25a_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày tập thể thao nam A-EBUFFER Anta 812317719-7</a>
+                    <p className="text-red-500">860,000₫</p>
+                    <s>1,599,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-46%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/822317719-1-2_ebf10860d7884b66b0c8f59c76793e8f_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày tập thể thao nữ A-EBUFFER Anta 822317719-1</a>
+                    <p className="text-red-500">860,000₫</p>
+                    <s>1,599,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-50%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812238080-2__25__53b3965d57924d9db990adda0ceaaaf2_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày chạy thể thao nam A-SILO Anta 812315555-4</a>                  <p className="text-red-500">1,080,000₫</p>
+                    <s>1,599,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-49%</button></s>
+                </div>
+            </div>
+            <h1 className=" px-14 font-semibold text-3xl pt-20">SẢN PHẨM MỚI</h1>
+            <div className="flex px-12">
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812325584s-2__17__e202ddb127a74af0aee83e32f525050d_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày chạy thể thao nam A-SILO Anta 812315555-4</a>
+                    <p className="text-red-500">1,799,100₫</p>
+                    <s>3,209,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-47%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812315586-10__20__e18efc711f0047c09750da28e2e07e02_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày tập thể thao nam A-EBUFFER Anta 812317719-7</a>
+                    <p className="text-red-500">1,799,100₫</p>
+                    <s>3,209,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-47%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812325585-__2__c535cc568dc24f60be44907cbe65f7df_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày tập thể thao nữ A-EBUFFER Anta 822317719-1</a>
+                    <p className="text-red-500">1,799,100₫</p>
+                    <s>3,209,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-47%</button></s>
+                </div>
+                <div>
+                    <img className="cursor-pointer hover:scale-105 duration-300 ease-linear" src="https://product.hstatic.net/1000150581/product/812325585-2__4__2d9be1b3048642d187264f65732e8c52_large.jpg" alt="" />
+                    <a className="hover:text-blue-600 font-semibold">Giày chạy thể thao nam A-SILO Anta 812315555-4</a>
+                    <p className="text-red-500">1,799,100₫</p>
+                    <s>3,209,000₫ <button className="bg-red-600 text-white rounded-lg px-1 text-sm ml-2">-47%</button></s>
+                </div>
+            </div>
+            {/*CONTENT */}
+
+            {/*FOOTER */}
+            <div className="flex bg-slate-950 text-white justify-around p-5 mt-10 items-center ">
+                <div className="leading-loose line-clamp-6">
+                    <b className="text-xl"> CÔNG TY CỔ PHẦN AN VIỆT SÔNG HỒNG</b><br />
+                    Địa chỉ: Số 5, ngõ 75, đường Hồng <br />
+                    Hà, Phường Phúc Xá, Quận Ba Đình, <br />
+                    Thành phố Hà Nội<br />
+                    Số điện thoại: 0974945488<br />
+                    Email: online@arr.com.vn<br />
+                    © Bản quyền thuộc về Anta Việt Nam |<br />
+                    Cung cấp bởi Haravan
+                </div>
+                <div className="leading-loose">
+                    <b>CHÍNH SÁCH</b> <br />
+                    Chính sách vận chuyển<br />
+                    Bảo hành/bảo trì<br />
+                    Đổi/trả hàng<br />
+                    Bảo mật thông tin
+                </div>
+                <div className="leading-loose">
+                    <b> HỖ TRỢ KHÁCH HÀNG</b><br />
+                    Giới thiệu<br />
+                    Quy định chung<br />
+                    Hình thức thanh toán<br />
+                    HỆ THỐNG CỬA HÀNG
+                </div>
+                <div>
+                    <i href="" className="font-bold text-2xl">SPORTVN</i>
+                </div>
+            </div>
+            {/*FOOTER */}
+        </div>
+    )
+}
