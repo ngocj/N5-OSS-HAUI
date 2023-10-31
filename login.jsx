5f7f4cb69f30b68b8a705df522cbcaddc235572b
Nguyễn Bảo Ngọc <91652961+ngocj@users.noreply.github.com>
2023-10-31 23:05:04 +0700

    đăng nhập, đăng kí , quên mật khẩu
    đăng nhập, đăng kí , quên mật khẩu

--- login.jsx
+++ login.jsx
@@ -1,51 +1,47 @@
 
-import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-
 export const Login = () => {
-    const [email, setEmail] = useState('');
+    const [username, setEmail] = useState('');
     const [password, setPassWord] = useState('');
     const navigate = useNavigate();
-    const handleLogin = async (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
-        try {
-            if (email.length === 0 || password.length === 0) {
-                alert("Không được để trống");
-            } else {
-                const res = await axios.post("https://reqres.in/api/login", {
-                    email,
-                    password,
-                });
-                const token = res.data.token;
-                if (token) {
-                    localStorage.setItem('token', token);
-                    alert("ĐĂNG NHẬP THÀNH CÔNG")
-                    navigate('/');
-                } else {
-                    alert("không có token");
-                }
-            }
-        } catch (error) {
-            console.error(error);
+        if (username === "admin" && password === "admin") {
+            navigate('/quanly');
+        }
+        else if (username === "" || password === "") {
+            alert("Vui lòng điền thông tin!")
+        }
+        else {
+            navigate('/');
         }
     };
+    const handleRegister = (e) => {
+        e.preventDefault();
+        navigate('/dangky')
+    }
+    const forgot = (e) => {
+        e.preventDefault();
+        navigate('/forgot')
+    }
     return (
-        <div className="h-screen bg-gradient-to-r from-indigo-500 pt-32">
-            <form className=" w-1/3 mx-auto font-sans bg-white px-12 py-10 rounded-lg shadow-md" onSubmit={handleLogin}>
+        <div className=" pt-20 ">
+            <form className=" w-2/5 mx-auto font-sans bg-white px-12 py-10 " onSubmit={handleLogin}>
                 <div className="text-center">
                     <h1 className="text-3xl font-semibold">ĐĂNG NHẬP TÀI KHOẢN</h1>
+                    <p>Bạn chưa có tài khoản ? <a href="" className="text-blue-500"  onClick={handleRegister}>Đăng ký tại đây</a></p>
                 </div>
                 <div className="mt-8">
-                    <label htmlFor="" className="block font-semibold">Email</label>
-                    <input type="email" className=" mb-4 w-full p-2 rounded-md border border-gray-300"
-                        placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                    <label htmlFor="" className="block font-semibold">Mật khẩu</label>
-                    <input type="password" className="mb-4 w-full p-2 rounded-md border border-gray-300"
-                        placeholder="Mật khẩu" value={password} onChange={e => setPassWord(e.target.value)} />
-                    <button type="submit" className="bg-blue-500 text-white w-full 
-                        py-2 rounded-md hover:bg-blue-600 duration-300 ease-in-out" >
+                    <label htmlFor="" className="block font-semibold">Username</label>
+                    <input type="text" className=" mb-4 w-full p-2 rounded-md border border-gray-300"
+                        placeholder="User name" value={username} onChange={e => setEmail(e.target.value)} />
+                    <label htmlFor="" className="block font-semibold">PassWord</label>
+                    <input type="password" className="mb-2 w-full p-2 rounded-md border border-gray-300"
+                        placeholder="Pass word" value={password} onChange={e => setPassWord(e.target.value)} />
+                    <p className="pb-2 text-sm">Quên mật khẩu ? Nhấn vào <a onClick={forgot} href="" className="text-blue-500">đây</a></p>
+                    <button type="submit" className="bg-black text-white w-full 
+                        py-2 rounded-md hover:bg-blue-600 duration-300 ease-in-out">
                         Đăng nhập
                     </button>
                 </div>
